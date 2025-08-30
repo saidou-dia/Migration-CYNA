c97704e3b2f5d8e823747e3ba7d238dcf65b738b
saidou dia <duuzey@hotmail.com>
2025-08-30 14:05:26 +0200

    backend change

--- 04-devops/01-terraform/backend.tf
+++ 04-devops/01-terraform/backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"     # RG où sera ton storage account
-    storage_account_name = "stterraformstate"      # Storage Account existant ou à créer
-    container_name       = "tfstate"               # Conteneur blob pour stocker l’état
-    key                  = "network.tfstate"      # Nom du fichier tfstate
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformsaidoudia2025"
+    container_name       = "tfstate"
+    key                  = "network.tfstate"
   }
 }
