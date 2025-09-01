f2d789c61f8f882161e01e53a8e12d60ccc117c9
saidou dia <duuzey@hotmail.com>
2025-09-02 00:14:53 +0200

    modul

--- 04-devops/01-terraform/modules/azure/nic/variables.tf
+++ 04-devops/01-terraform/modules/azure/nic/variables.tf
@@ -8,12 +8,3 @@ variable "resource_group" {
   type        = string
 }
 
-variable "subnet_virtualinfra_id" {
-  description = "ID du subnet VirtualInfra pour la NIC 1"
-  type        = string
-}
-
-variable "subnet_users_id" {
-  description = "ID du subnet Users pour la NIC 2"
-  type        = string
-}
